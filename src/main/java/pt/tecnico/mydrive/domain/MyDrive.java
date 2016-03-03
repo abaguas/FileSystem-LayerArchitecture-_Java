5827d2e6442417af3ad1ec6195068d53f35e5bf5
luissacouto <luis.sa.couto@ist.utl.pt>
2016-03-03 17:04:11 +0000

    Actualização do MyDrive com imports e pequenas correcções.

--- src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
+++ src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
@@ -1,6 +1,8 @@
 package pt.tecnico.mydrive.domain;
 
 import pt.tecnico.mydrive.exception.MyDriveException;
+import pt.tecnico.mydrive.exception.FileOrDirectoryAlreadyExistsException;
+import pt.tecnico.mydrive.exception.NoSuchFileOrDirectoryException;
 
 public class MyDrive extends MyDrive_Base {
     
@@ -16,15 +18,15 @@ public class MyDrive extends MyDrive_Base {
 	getRootUser.setMainDirectory(getCurrentDir());
     }
 
-    public void createDirectory(String name) throws DirectoryAlreadyExistsException{
+    public void createDirectory(String name) throws FileOrDirectoryAlreadyExistsException{
 	getCurrentDir().createDir(name, getCurrentUser());
     }
 
-    public void cd(String name) throws NoSuchFileOrDirectory {
+    public void cd(String name) throws NoSuchFileOrDirectoryException {
 	setCurrentDir(getCurrentDir().get(name));
     }
 
-    public void ls(String name){
+    public void ls(String name) throws NoSuchFileOrDirectoryException{
 	getCurrentDir().ls(name);
     }
     
