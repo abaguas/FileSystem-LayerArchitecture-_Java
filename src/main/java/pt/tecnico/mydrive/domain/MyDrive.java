b8d9c4f1b1fcfe747abd1cc6c7bdb06bc6f2228f
luissacouto <luis.sa.couto@ist.utl.pt>
2016-03-03 16:31:39 +0000

    Primeiras linhas do MyDrive feitas.

--- src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
+++ src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
@@ -1,9 +1,24 @@
 package pt.tecnico.mydrive.domain;
 
+import pt.tecnico.mydrive.exception.MyDriveException;
+
 public class MyDrive extends MyDrive_Base {
     
-    public MyDrive() {
+    public MyDrive() throws MyDriveException{
         super();
+	setRootUser(new Root ());
+	setCurrentUser(getRootUser());
+	setRootDirectory(new Directory("/", getRootUser()));
+	getRootDirectory().create("home", getRootUser());
+	cd("home");
+	getRootDirectory().create("root", getRootUser());
+	cd("root");
+	getRootUser.setMainDirectory(getCurrentDir());
     }
+
+    public createDirectory(String name){
+	getCurrentDir().createDir(name, getCurrentUser());
+    }
+
     
 }
