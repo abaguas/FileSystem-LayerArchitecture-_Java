3e74fb0d79ec2510f9d09a8dc19d28af3756dbd6
Andre Aguas <andre.b.aguas@gmail.com>
2016-03-05 12:34:45 +0000

    Correcao do getName

--- src/main/java/pt/tecnico/mydrive/exception/NoSuchFileOrDirectoryException.java
+++ src/main/java/pt/tecnico/mydrive/exception/NoSuchFileOrDirectoryException.java
@@ -1,19 +1,20 @@
 package pt.tecnico.mydrive.exception;
 
 public class NoSuchFileOrDirectoryException extends MyDriveException {
-	
+
     private static final long serialVersionUID = 1L;
-	
+
     private String _name;
-	
+
 	public NoSuchFileOrDirectoryException(String name){
 		_name=name;
 	}
-    
+	
+	//_name em vez de contactName
     public String getName() {
-        return contactName;
+        return _name;
     }
-    
+
     @Override
     public String getMessage(){
         return getName() + " : No Such File or Directory.";
