76ef608b3bbfd2b0301ee40e952ff9c6c695f775
davidmfer <david.mesquita.fernandes@gmail.com>
2016-03-06 23:38:52 +0000

    Update PlainFile.java

--- src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
+++ src/main/java/pt/tecnico/mydrive/domain/PlainFile.java
@@ -1,9 +1,35 @@
 package pt.tecnico.mydrive.domain;
 
+import org.jdom2.Element;
+import org.jdom2.Document;
+
 public class PlainFile extends PlainFile_Base {
     
     public PlainFile() {
         super();
+    }
+    
+    @Override
+    public void XMLExport(Element element_mydrive){
+        Element element = new Element ("plain");
+        //element.setAttribute("id",getId());
+        
+        element = new Element ("path");
+        //element.setText(getPath());
+        
+        element = new Element ("name");
+        element.setText(getName());
+        
+        element = new Element ("owner");
+        element.setText(getOwner().getUsername());
+        
+        element = new Element ("perm");
+        //element.setText(getPerm());
+        
+        element = new Element ("contents");
+        element.setText (getContent());
+        
+        element_mydrive.addContent(element);
     }
     
 }
