ee6d2af472ec7fcc0911611af42f2115e3518e6d
davidmfer <david.mesquita.fernandes@gmail.com>
2016-03-06 23:37:57 +0000

    Update Link.java

--- src/main/java/pt/tecnico/mydrive/domain/Link.java
+++ src/main/java/pt/tecnico/mydrive/domain/Link.java
@@ -1,9 +1,35 @@
 package pt.tecnico.mydrive.domain;
 
+import org.jdom2.Element;
+import org.jdom2.Document;
+
 public class Link extends Link_Base {
     
     public Link() {
         super();
+    }
+    
+    @Override
+    public void XMLExport(Element element_mydrive){
+        Element element = new Element ("link");
+        //element.setAttribute("id",getId());
+        
+        element = new Element ("path");
+        //element.setText(getPath());
+        
+        element = new Element ("name");
+        element.setText(getName());
+        
+        element = new Element ("owner");
+        element.setText(getOwner().getUsername());
+        
+        element = new Element ("perm");
+        //element.setText(getPerm());
+        
+        element = new Element ("value");
+        //element.setText (getValue());
+        
+        element_mydrive.addContent(element);
     }
     
 }
