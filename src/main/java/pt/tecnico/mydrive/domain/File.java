43a82d392edd4256dc8262e89d1a5f8363da57ae
davidmfer <david.mesquita.fernandes@gmail.com>
2016-03-06 23:36:57 +0000

    Update File.java

--- src/main/java/pt/tecnico/mydrive/domain/File.java
+++ src/main/java/pt/tecnico/mydrive/domain/File.java
@@ -1,5 +1,8 @@
 package pt.tecnico.mydrive.domain;
 
+import org.jdom2.Element;
+import org.jdom2.Document;
+
 public class File extends File_Base {
     
     public File() {
@@ -17,4 +20,7 @@ public class File extends File_Base {
     	//TODO
     	return s;
     }
+    
+    public void XMLExport(Element element_mydrive){}
+    
 }
