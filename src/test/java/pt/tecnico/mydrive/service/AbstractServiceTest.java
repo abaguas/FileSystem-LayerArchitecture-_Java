5658dd9255f0ad8e3fec97ee9973132c9b9c6168
luissacouto <luis.sa.couto@ist.utl.pt>
2016-04-02 19:41:35 +0100

    Classe Abstracta de testes -> Trabalho conjunto.

---
+++ src/test/java/pt/tecnico/mydrive/service/AbstractServiceTest.java
@@ -0,0 +1,42 @@
+package pt.tecnico.phonebook.service;
+
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.core.WriteOnReadError;
+import pt.tecnico.mydrive.MyDriveApplication;
+
+public abstract class AbstractServiceTest {
+
+    @BeforeClass // run once berfore each test class
+    public static void setUpBeforeAll() throws Exception {
+	// run tests with a clean database!!!
+	   //MyDriveApplication.init();
+    }
+
+    @Before // run before each test
+    public void setUp() throws Exception {
+        try {
+            FenixFramework.getTransactionManager().begin(false);
+            populate();
+        } catch (WriteOnReadError | NotSupportedException | SystemException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @After // rollback after each test
+    public void tearDown() {
+        try {
+            FenixFramework.getTransactionManager().rollback();
+        } catch (IllegalStateException | SecurityException | SystemException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void populate(); // each test adds its own data
+}
