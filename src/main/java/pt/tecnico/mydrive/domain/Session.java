f1d1d48f6af7a485d8ef552711fb9d0c2a8f6d4c
luissacouto <luis.sa.couto@ist.utl.pt>
2016-04-04 13:25:29 +0100

    Inserção de argumento token em todos os métodos funcionais do mydrive

--- src/main/java/pt/tecnico/mydrive/domain/Session.java
+++ src/main/java/pt/tecnico/mydrive/domain/Session.java
@@ -2,8 +2,11 @@ package pt.tecnico.mydrive.domain;
 
 public class Session extends Session_Base {
     
-    public Session() {
+    public Session(User u, long token, Login login) {   //Login
         super();
+        setCurrentUser(u);
+        setToken(token);
+        setLogin(login);
     }
     
 }
