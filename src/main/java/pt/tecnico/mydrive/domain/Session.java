d679dfa1ff7545a0d8e2d7e9b676122101e807e0
Andre Aguas <andre.b.aguas@gmail.com>
2016-04-05 11:39:49 +0100

    remocao da classe login(comentada para migracao), FIXME em Session e MyDrive

--- src/main/java/pt/tecnico/mydrive/domain/Session.java
+++ src/main/java/pt/tecnico/mydrive/domain/Session.java
@@ -2,11 +2,10 @@ package pt.tecnico.mydrive.domain;
 
 public class Session extends Session_Base {
     
-    public Session(User u, long token, Login login) {   //Login
+    public Session(User u, long token) {   //Login //FIXME retirei o argumento login que era recebido
         super();
         setCurrentUser(u);
         setToken(token);
-        setLogin(login);
     }
     
 }
