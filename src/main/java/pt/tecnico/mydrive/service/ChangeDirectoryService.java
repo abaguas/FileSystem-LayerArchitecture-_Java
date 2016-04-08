98c80de9663901c1e7832a52a75481658169d59c
netjinho <neto_moises95@hotmail.com>
2016-04-08 23:05:24 +0100

    criados servico e test de CD (vazios)

---
+++ src/main/java/pt/tecnico/mydrive/service/ChangeDirectoryService.java
@@ -0,0 +1,13 @@
+package pt.tecnico.mydrive.service;
+
+import pt.tecnico.mydrive.exception.MyDriveException;
+
+public class ChangeDirectoryService extends MyDriveService{
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
