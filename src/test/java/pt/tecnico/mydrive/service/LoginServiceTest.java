1e12f76b528b862e24d76abde8c3899801d99366
davidmfer <david.mesquita.fernandes@gmail.com>
2016-04-11 05:35:33 -0700

    Create LoginServiceTest.java

---
+++ src/test/java/pt/tecnico/mydrive/service/LoginServiceTest.java
@@ -0,0 +1,54 @@
+/*package pt.tecnico.mydrive.service;
+
+public class LoginTest extends AbstractServiceTest{
+	protected void populate(){
+
+		MyDrive md = MyDrive.getInstance();
+		User u1= new User("Super User", "***", "root");
+		Session s1 = new Session (u1, md);
+
+		User u2 = new User ("Super User", "****", "root");
+		Session s2= new Session (u2, md);
+		
+		User u3 = New User ("Non Existent", "****", "NonExistent");
+		Session s3= new Session(u3, md);
+		
+		Session s4 = new Session(u1, md);
+		int time = s4.getTime().getTimestamp();
+/*		if time != 0 ->Erro */
+		
+		Session s5 = new Session (u1, md);
+		int time = s5.getTime().getTimestamp();
+/*		if time != 0 ->Erro */
+
+		Session s6 = new Session (u1, md);
+		int time = s6.getTime().getTimestamp();
+		
+
+
+	@Test
+	public void SuccessfulLogin(){
+		 //Sucesso
+	}
+	
+	@Test(expected = InvalidUserOrPasswordException.class)
+		public void UnsuccessfulLoginDueToWrongPassword(){
+ //Erro
+		}
+	
+	@Test(expected = InvalidUserOrPasswordException.class)
+		public void UnsuccessfulLoginDueToNonExistentUser(){
+//Erro
+		}
+	
+	
+	@Test
+	public void initiateSessionPeriod(){
+	}
+	
+	@Test
+	public void reinitiateSessionPeriod(){
+}
+*/
+	
+		
