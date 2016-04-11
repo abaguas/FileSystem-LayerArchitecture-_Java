1b2ca85a9bd55cb6bb267286b5e0752de4c0841b
davidmfer <david.mesquita.fernandes@gmail.com>
2016-04-11 05:46:41 -0700

    Update LoginServiceTest.java

--- src/test/java/pt/tecnico/mydrive/service/LoginServiceTest.java
+++ src/test/java/pt/tecnico/mydrive/service/LoginServiceTest.java
@@ -4,40 +4,27 @@ public class LoginTest extends AbstractServiceTest{
 	protected void populate(){
 
 		MyDrive md = MyDrive.getInstance();
-		User u1= new User("Super User", "***", "root");
-		Session s1 = new Session (u1, md);
+		User u1= new User("Utilizador1", "1234", "user1");
 
-		User u2 = new User ("Super User", "****", "root");
-		Session s2= new Session (u2, md);
-		
-		User u3 = New User ("Non Existent", "****", "NonExistent");
-		Session s3= new Session(u3, md);
-		
-		Session s4 = new Session(u1, md);
-		int time = s4.getTime().getTimestamp();
-		if time != 0 ->Erro 
-		
-		Session s5 = new Session (u1, md);
-		int time = s5.getTime().getTimestamp();
-		if time != 0 ->Erro 
 
-		Session s6 = new Session (u1, md);
-		int time = s6.getTime().getTimestamp();
-		
-
+	}
 
 	@Test
 	public void SuccessfulLogin(){
+		long token = LoginService("user1", "1234");
+		assertNotNull (token);
 		 //Sucesso
 	}
 	
 	@Test(expected = InvalidUserOrPasswordException.class)
-		public void UnsuccessfulLoginDueToWrongPassword(){
+		public void UnsuccessfulLoginDueToWrongPassword() throws InvalidUserOrPasswordException{
+		long token = LoginService("user1", "1235");
  //Erro
 		}
 	
 	@Test(expected = InvalidUserOrPasswordException.class)
-		public void UnsuccessfulLoginDueToNonExistentUser(){
+		public void UnsuccessfulLoginDueToNonExistentUser() throws InvalidUserOrPasswordException{
+		long token = LoginService ("NonExistent", "1243");
 //Erro
 		}
 	
@@ -48,7 +35,7 @@ public class LoginTest extends AbstractServiceTest{
 	
 	@Test
 	public void reinitiateSessionPeriod(){
-}
+	}
+
+}	
 */
-	
-		
