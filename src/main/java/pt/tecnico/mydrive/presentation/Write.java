92eb6e98c78f4d9e45bacda570caf5a938599a8c
netjinho <neto_moises95@hotmail.com>
2016-04-30 08:04:02 +0100

    Shell, Command & Write Command

---
+++ src/main/java/pt/tecnico/mydrive/presentation/Write.java
@@ -0,0 +1,19 @@
+package pt.tecnico.mydrive.presentation;
+
+import pt.tecnico.mydrive.service.WriteFileService;
+
+public class Write extends MyDriveCommand {
+
+	public Write(Shell sh) {
+		super(sh, "update", "change the content of a file");
+	}
+
+	@Override
+	void execute(String[] args) {
+		if (args.length < 2 || args.length > 2)
+		    throw new RuntimeException("USAGE: "+name()+" <path> <text>");
+		else
+		    new WriteFileService(args[0], args[1], 1).execute(); //FIXME token is third parameter
+	}
+
+}
