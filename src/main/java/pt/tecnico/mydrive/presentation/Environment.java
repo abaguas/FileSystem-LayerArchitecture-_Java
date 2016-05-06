b15b1f90f6c33633bd95fc9c204ca54dbdd0ba99
abaguas <andre.b.aguas@gmail.com>
2016-05-06 14:04:28 +0100

    environment command done. Only waiting for the service to do proper imports

---
+++ src/main/java/pt/tecnico/mydrive/presentation/Environment.java
@@ -0,0 +1,39 @@
+package pt.tecnico.mydrive.presentation;
+
+import pt.tecnico.mydrive.service.LoginService;
+
+public class Environment extends MyDriveCommand {
+	
+	public Environment(Shell sh) {
+		super(sh, "env", "create or change environment variable");
+	}
+
+	@Override
+	void execute(String[] args) {
+		if (args.length < 0 || args.length > 2) {
+		    throw new RuntimeException("USAGE: " + name() +" [<name> [<value>]]");
+		}
+		else {
+			long token = new LoginService(username, password).execute();
+			AddVariableService avs = null;
+			if (args == 0) {
+				new AddVariableService(token, null, null);
+				avs.execute();
+			}
+			else if (args == 1){
+				new AddVariableService(token, args[0], null);
+				avs.execute();
+			}
+			else {
+				avs = new AddVariableService(token, args[0], args[1]);
+				avs.execute();
+			}
+		}
+		for (VariableDto v: avs.result()) {
+			System.out.println(avs.getName()+" = "+avs.getValue();
+		}
+	}
+		
+	
+
+}
