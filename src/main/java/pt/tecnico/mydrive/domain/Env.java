44beb27eb60dbcd8c268bd0d9e6679e2aafd1876
filipencosta <filipe.n.costa@tecnico.ulisboa.pt>
2016-05-10 22:37:02 +0100

    Variaveis ambiente

--- src/main/java/pt/tecnico/mydrive/domain/Env.java
+++ src/main/java/pt/tecnico/mydrive/domain/Env.java
@@ -3,7 +3,8 @@ package pt.tecnico.mydrive.domain;
 public class Env extends Env_Base {
     
     public Env(String name, String value) {
-        super();
+        super.setName(name);
+        super.setValue(values);
     }
 
     @Override
