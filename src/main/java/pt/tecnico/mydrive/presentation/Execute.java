38fff46544b0960b51821f954725b318094b39d8
filipencosta <filipe.n.costa@tecnico.ulisboa.pt>
2016-05-11 18:33:09 +0100

    Create Execute.java
    Comando Execute

---
+++ src/main/java/pt/tecnico/mydrive/presentation/Execute.java
@@ -0,0 +1,25 @@
+package pt.tecnico.mydrive.presentation;
+
+import pt.tecnico.mydrive.service.ExecutePlainFileService;
+
+public class Execute extends MyDriveCommand {
+	public Execute(MyDriveShell sh){
+		super(sh, "do", "Execute a Plain File");
+	}
+
+	public void execute(String[] args){
+		if(args.length > 1){
+		  long token = shell().getActiveToken();
+		  String path = args[0];
+		  String[] arguments;
+		  for(int i=1; i < args.length ; i++){
+		    arguments[i-1] = args[i];
+		  }
+		  ExecutePlainFileService eps = new ExecutePlainFileService(token, path, arguments);
+			eps.execute();
+		}
+		else{
+			throw new RuntimeException("USAGE: " + name() + " [<path>]");
+		}
+	}
+}
